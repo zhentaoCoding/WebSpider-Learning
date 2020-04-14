5bc1405e5ae8cecf8445f4c6cbdfef585f4fdb96
zhentaoCoding <zt950607@gmail.com>
2020-04-14 16:25:13 +0800

    update

---
+++ selenium_project.py
@@ -0,0 +1,89 @@
+from selenium import webdriver
+from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.wait import WebDriverWait
+import logging
+from urllib.parse import urljoin
+from os import makedirs
+from os.path import exists
+import json
+
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s: %(message)s')
+INDEX_URL = 'https://dynamic2.scrape.cuiqingcai.com/page/{page}'
+TIMEOUT = 10
+TOTAL_PAGE = 10
+
+
+brower = webdriver.Safari()
+wait = WebDriverWait(brower, TIMEOUT)
+
+
+def scrape_page(url, condition, locator):
+    logging.info('scraping %s', url)
+    try:
+        brower.get(url)
+        wait.until(condition(locator))
+    except TimeoutException:
+        logging.error('error occurred while scraping %s', url, exc_info=True)
+
+
+def scrape_index(page):
+    url = INDEX_URL.format(page=page)
+    scrape_page(url, condition=EC.visibility_of_all_elements_located, locator=(By.CSS_SELECTOR, '#index .item'))
+
+
+def parse_index():
+    elements = brower.find_elements_by_css_selector('#index .item .name')
+    for element in elements:
+        href = element.get_attribute('href')
+        yield urljoin(INDEX_URL, href)
+
+
+def scrape_detail(url):
+    scrape_page(url, condition=EC.visibility_of_element_located, locator=(By.TAG_NAME, 'h2'))
+
+
+def parse_detail():
+    url = brower.current_url
+    name = brower.find_element_by_tag_name('h2').text
+    categories = [element.text for element in brower.find_elements_by_css_selector('.categories button span')]
+    cover = brower.find_element_by_css_selector('.cover').get_attribute('src')
+    score = brower.find_element_by_class_name('score').text
+    drama = brower.find_element_by_css_selector('.drama p').text
+    return {
+        'url': url,
+        'name': name,
+        'categories': categories,
+        'cover': cover,
+        'score': score,
+        'drama': drama
+    }
+
+
+RESULTS_DIR = 'results'
+exists(RESULTS_DIR) or makedirs(RESULTS_DIR)
+
+
+def save_data(data):
+    name = data.get('name')
+    data_path = f'{RESULTS_DIR}/{name}.json'
+    json.dump(data, open(data_path, 'w', encoding='utf-8'), ensure_ascii=False, indent=2)
+
+def main():
+    try:
+        for page in range(1,TOTAL_PAGE + 1):
+            scrape_index(page)
+            detail_urls = parse_index()
+            for detail_url in list(detail_urls):
+                logging.info('get detail url %s', list(detail_url))
+                scrape_detail(detail_url)
+                detail_data = parse_detail()
+                logging.info('detail data %s', detail_data)
+    finally:
+        brower.close()
+
+
+if __name__ == '__main__':
+    main()
